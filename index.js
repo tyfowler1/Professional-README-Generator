994030bf3c47a62ef8be45f11fd7e24eeb923b31
tyfowler1 <145846973+tyfowler1@users.noreply.github.com>
2023-12-06 13:31:21 -0500

    added code

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  { type: 'input', name: 'title', message: 'Enter your project title:' },
+  { type: 'input', name: 'description', message: 'Enter a description:' },
+  { type: 'input', name: 'installation', message: 'Enter installation instructions:' },
+  { type: 'input', name: 'usage', message: 'Enter usage information:' },
+  { type: 'input', name: 'contributing', message: 'Enter contribution guidelines:' },
+  { type: 'input', name: 'tests', message: 'Enter test instructions:' },
+  { type: 'list', name: 'license', message: 'Choose a license:', choices: ['MIT', 'GPLv3', 'Apache', 'None'] },
+  { type: 'input', name: 'username', message: 'Enter your GitHub username:' },
+  { type: 'input', name: 'email', message: 'Enter your email address:' },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer.prompt(questions).then(answers => {
+    const readmeContent = `
+  # ${answers.title}
+  
+  ![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)
+  
+  ## Description
+  
+  ${answers.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${answers.installation}
+  
+  ## Usage
+  
+  ${answers.usage}
+  
+  ## License
+  
+  This project is covered under the ${answers.license} license.
+  
+  ## Contributing
+  
+  ${answers.contributing}
+  
+  ## Tests
+  
+  ${answers.tests}
+  
+  ## Questions
+  
+  For any questions, please reach out to [${answers.username}](https://github.com/${answers.username}) or [email me](mailto:${answers.email}).
+    `;
+  
+    fs.writeFile('README.md', readmeContent, err => {
+      if (err) throw err;
+      console.log('README.md has been generated!');
+    });
+  });
